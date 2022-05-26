8381433b3e91565a5213991d51f43834a319c8de
Dinh Loc <C:/Program File/Notepad++/notepad++.exe' -mutiInst -notabbar - nosession -noPlugin>
2022-05-26 19:46:38 +0700

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnchon = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtMaHoa = new System.Windows.Forms.TextBox();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnchon
+            // 
+            this.btnchon.Location = new System.Drawing.Point(56, 42);
+            this.btnchon.Name = "btnchon";
+            this.btnchon.Size = new System.Drawing.Size(94, 29);
+            this.btnchon.TabIndex = 0;
+            this.btnchon.Text = "Chọn";
+            this.btnchon.UseVisualStyleBackColor = true;
+            this.btnchon.Click += new System.EventHandler(this.Folder_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(186, 42);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(224, 27);
+            this.txtFile.TabIndex = 1;
+            // 
+            // txtMaHoa
+            // 
+            this.txtMaHoa.Location = new System.Drawing.Point(186, 100);
+            this.txtMaHoa.Name = "txtMaHoa";
+            this.txtMaHoa.Size = new System.Drawing.Size(224, 27);
+            this.txtMaHoa.TabIndex = 2;
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Location = new System.Drawing.Point(56, 100);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(94, 29);
+            this.btnMaHoa.TabIndex = 3;
+            this.btnMaHoa.Text = "Mã hóa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.Mahoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMaHoa);
+            this.Controls.Add(this.txtMaHoa);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnchon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnchon;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtMaHoa;
+        private System.Windows.Forms.Button btnMaHoa;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFile.Text = diag.SelectedPath;
+            }
+            else
+                txtFile.Text = "";
+        }
+
+        private void Mahoa_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            string result = "";
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            buffer = md5.ComputeHash(buffer);
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                result += buffer[i].ToString("x2");
+            }
+            txtMaHoa.Text = result;
+        }
+    }
+}
